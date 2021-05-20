7f0a6ae9ad39676b305fdddb850feb5828ec3230
Yuri Valentini <yv@3cx.com>
2021-05-20 11:46:46 +0200

    coverage dello sconto TwoForAmount

--- SuperMarket/SuperMarketTest.cpp
+++ SuperMarket/SuperMarketTest.cpp
@@ -21,16 +21,20 @@ TEST_CASE("SingleProductReceipt", "[Supermarket]")
     catalog->addProduct(apples, 1.99);
     Product soap("soap", ProductUnit::Each);
     catalog->addProduct(soap, 0.59);
+    Product shampoo("shampoo", ProductUnit::Each);
+    catalog->addProduct(shampoo, 1.19);
 
     Teller teller(catalog);
     teller.addSpecialOffer(SpecialOfferType::TenPercentDiscount, toothbrush, 10.0);
     teller.addSpecialOffer(SpecialOfferType::ThreeForTwo, soap, 0.0);
+    teller.addSpecialOffer(SpecialOfferType::TwoForAmount, shampoo, 2.0);
 
     ShoppingCart cart;
     cart.addItem(toothbrush);
     cart.addItemQuantity(apples, 2.5);
     cart.addItem(toothbrush);
     cart.addItemQuantity(soap, 3);
+    cart.addItemQuantity(shampoo, 2);
 
     // ACT
     Receipt receipt = teller.checksOutArticlesFrom(cart);
